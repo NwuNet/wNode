c9b29499fc8ac4cf028256b957b81f1acf470206
wo646231194 <wo646231194@live.com>
2016-09-12 23:11:45 +0800

    2016-09-12 webview添加mask 和 loading 效果

--- index.js
+++ index.js
@@ -5,3 +5,11 @@
 // })
 
 nav1click()
+
+var webview = document.getElementById('webcontent')
+webview.addEventListener('did-start-loading', ()=>{
+    document.getElementById("mask").style.display ="block";
+})
+webview.addEventListener('did-stop-loading',()=>{
+    document.getElementById("mask").style.display ="none";
+})
